062a3851f1d38bc2b0e03539b601444f9f62d3c9
zhangyi <zhangyi@2345.com>
2018-12-25 20:55:53 +0800

    添加头文件

---
+++ fastcgi.h
@@ -0,0 +1,13 @@
+//
+// Created by zhangyi on 2018/12/25.
+//
+
+#ifndef FASTCGI_DEMO_FASTCGI_H
+#define FASTCGI_DEMO_FASTCGI_H
+
+void haltError(const char *, int);
+
+
+
+
+#endif //FASTCGI_DEMO_FASTCGI_H
